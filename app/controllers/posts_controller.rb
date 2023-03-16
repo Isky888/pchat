0df0f51a580e2f1fb408d81e9ecb1667fb37ad2e
Iskander Galbraith <ijgalbraith@protonmail.ch>
2023-03-16 19:57:01 +0000

    new posts working

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,5 +4,22 @@ class PostsController < ApplicationController
   end
 
   def create
+    @project = Project.find(params[:project_id])
+    @post = Post.new(post_params)
+    @post.user = current_user
+    @post.project = @project
+    if @post.save
+      redirect_to project_path(@project)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content, :project)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   resources :projects do
-    resources :posts
+    resources :posts, only: [:new, :create, :index]
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
